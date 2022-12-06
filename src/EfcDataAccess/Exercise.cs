c03c3ce3c5f6e5d40d46b89b0a649db06d02720e
Ondrej Klimek <klimekondrej01@gmail.com>
2022-12-06 23:51:18 +0100

    'SEP3-53'

---
+++ src/EfcDataAccess/Exercise.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EfcDataAccess;
+
+public class Exercise
+{
+    [Key]
+    public int Id { get; set; }
+    //[Required, MinLength(3), MaxLength(30)]
+    public string Name { get; set; }
+    //[Required, MaxLength(500)]
+    public string Descritpion { get; set; }
+    //[Range(0, 600)]
+    public int DurationInMin { get; set; }
+    public ICollection<ExercisesInWorkouts> InWorkouts { get; set; }
+    
+    public UserTable User { get; set; }
+}
---
+++ src/EfcDataAccess/ExercisesInWorkouts.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EfcDataAccess;
+
+public class ExercisesInWorkouts
+{
+    [ForeignKey(nameof(Exercise))]
+    public int ExerciseId { get; set; }
+    [ForeignKey(nameof(Workout))]
+    public int WorkoutId { get; set; }
+    
+    public Exercise Exercise { get; set; }
+    public Workout Workout { get; set; }
+}

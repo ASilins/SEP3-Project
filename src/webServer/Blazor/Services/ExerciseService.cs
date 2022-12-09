856c9b08f6ae05f207f7a299ee528fbd50c3e2b2
Arturs Silins <80292989+ASilins@users.noreply.github.com>
2022-12-09 16:56:14 +0100

    SEP3-94

--- src/webServer/Blazor/Services/ExerciseService.cs
+++ src/webServer/Blazor/Services/ExerciseService.cs
@@ -1,4 +1,5 @@
 using System.Net.Http.Json;
+using System.Text.Json;
 using Model.DTOs;
 
 namespace Blazor.Services;
@@ -20,5 +21,31 @@ public class ExerciseService
         {
             throw new Exception(result);
         }
+    }
+
+    public async Task<List<ExerciseDTO>> GetExercises()
+    {
+        // This is for testing the UI without having any exercises
+        // var exercises = new List<ExerciseDTO>()
+        // {
+        //     new ExerciseDTO() { Id = 1, Name = "Exercise", Description = "Jump or something" , Duration = 1},
+        //     new ExerciseDTO() { Id = 2, Name = "Exercise2", Description = "Swim or something" , Duration = 2}
+        // };
+        // return Task.FromResult(exercises);
+
+        HttpResponseMessage response = await client.GetAsync("/exercises");
+        string result = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(result);
+        }
+
+        List<ExerciseDTO> exercises = JsonSerializer.Deserialize<List<ExerciseDTO>>(
+            result, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            })!;
+
+        return exercises;
     }
 }
